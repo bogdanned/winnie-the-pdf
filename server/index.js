ad80e9308752c9bcdf528a696885da86049b7902
Bogdan Mihai <bogdanned32@gmail.com>
2017-08-29 23:47:51 +0200

    express mocking server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const port = 3000
+
+const app = express()
+
+
+app.use('/', (req, res)=>{
+  res.send('Hello, is winnie the poh!')
+})
+
+
+app.listen(port, ()=>(console.log('Server running on port: ' + port))
